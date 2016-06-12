5af2ce5ae31a712540dec8e657d5134ebdf13ab5
Shin <kookie@amazon.com>
2016-06-12 21:19:40 +0100

    move key for cider-doc to `C-c s`

--- init/_clojure.el
+++ init/_clojure.el
@@ -12,6 +12,17 @@
       (call-interactively 'cider-jack-in)
     (cider-switch-to-repl-buffer)))
 
+(with-eval-after-load "cider-mode"
+  (when (boundp 'cider-mode-map)
+    ;; I used `C-c C-d' for `delete-chars-forward-with-syntax' years.
+    ;; Since xcscope.el uses `C-c s d' for similar purpose, I rebind
+    ;; `cider-doc-map' to `C-c s'.
+    (cinsk/move-key cider-mode-map [(control ?c) (control ?d)] [(control ?c) ?s])))
+
+(with-eval-after-load "cider-repl-mode"
+  (when (boundp 'cider-repl-mode-map)
+    (cinsk/move-key cider-repl-mode-map [(control ?c) (control ?d)] [(control ?c) ?s])))
+
 (with-eval-after-load "clojure-mode"
   (when (locate-library "paredit")
     (add-hook 'clojure-mode-hook 'paredit-mode))
