f608a45e61969504e1e3b6dde64a4cf4cc4f7470
cinsk <cinsky@gmail.com>
2017-11-26 20:42:11 -0800

    add js2-mode support

--- init/_js.el
+++ init/_js.el
@@ -5,17 +5,26 @@
 ;;;
 
 (eval-when-compile
-  (require 'js)
-  (require 'js-comint nil t))
+  (if (locate-library "js2-mode")
+      (require 'js2-mode)
+    (require 'js)
+    (require 'js-comint nil t)))
 
-(when (locate-library "js-comint")
-  (require 'js-comint))
 
-(let ((path (executable-find "js")))
-  (when path
-    (setq inferior-js-program-command path)))
+(if (locate-library "js2-mode")
+    (progn
+      (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+      (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+      (add-to-list 'interpreter-mode-alist '("nodejs" . js2-mode)))
 
-(with-eval-after-load "js"
-  (define-key js-mode-map [(control ?c) ?\!] 'run-js)
-  (define-key js-mode-map [(control ?c) (control ?r)] 'js-send-region-and-go)
-  (define-key js-mode-map [(control ?c) (control ?b)] 'js-send-buffer-and-go))
+  (when (locate-library "js-comint")
+    (require 'js-comint))
+
+  (let ((path (executable-find "js")))
+    (when path
+      (setq inferior-js-program-command path)))
+
+  (with-eval-after-load "js"
+    (define-key js-mode-map [(control ?c) ?\!] 'run-js)
+    (define-key js-mode-map [(control ?c) (control ?r)] 'js-send-region-and-go)
+    (define-key js-mode-map [(control ?c) (control ?b)] 'js-send-buffer-and-go)))
