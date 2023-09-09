2e99bae7778f4e9f34ef654b2055e62a446f9924
Seong-Kook Shin <cinsky@gmail.com>
2023-09-09 11:57:16 -0700

    lsp support: gopls configuration

--- init/_go.el
+++ init/_go.el
@@ -19,75 +19,84 @@
               (subword-mode 1))))
 
 (add-hook 'go-mode-hook 'cinsk/go-check-utils)
-
-(with-eval-after-load "go-mode"
-  (add-hook 'before-save-hook 'gofmt-before-save))
-
-;;
-;; Prefer company-mode based auto-completion to auto-complete-mode based one.
-;;
-(cond ((locate-library "company")
-       (add-hook 'go-mode-hook 'cinsk/go-init-cp-autocomplete))
-      ((locate-library "auto-complete")
-       (add-hook 'go-mode-hook 'cinsk/go-init-ac-autocomplete)))
-
-(defun cinsk/go-check-gocode (&optional nowarn)
-  (unless (executable-find "gocode")
-    (add-to-list 'exec-path (substitute-in-file-name "$GOPATH/bin"))
-    (unless (and (executable-find "gocode") nowarn)
-      (warn "gocode not found; try build it using 'go get github.com/nsf/gocode' in $GOPATH directory"))))
+(add-hook 'go-mode-hook 'lsp-deferred)
 
 (defun cinsk/go-check-utils (&optional nowarn)
-  (if (or (executable-find "goimports")
-          (let ((binpath (path-join (string-trim (shell-command-to-string "go env GOPATH") nil)
-                                    "bin")))
-            (let ((pname (path-join binpath "goimports")))
-              (if (file-executable-p pname)
-                  (add-to-list 'exec-path binpath)))))
-      (setq gofmt-command "goimports")
-    (unless nowarn
-      (warn "goimports not found; try build it using 'go get golang.org/x/tools/cmd/goimports' in $GOPATH directory"))))
+  (if (executable-find "gopls")
+      (progn
+        (add-hook 'before-save-hook #'lsp-format-buffer t t)
+        (add-hook 'before-save-hook #'lsp-organize-imports t t))
+      (unless nowarn
+        (warn "gopls not found; See https://github.com/golang/tools/tree/master/gopls for more"))))
 
+(when nil
+  (with-eval-after-load "go-mode"
+    (add-hook 'before-save-hook 'gofmt-before-save))
 
-(defun cinsk/go-init-cp-autocomplete ()
-  "Set up auto completion of Go sources using `company-mode'"
-  (unless (locate-library "company-go")
-    (let ((go-cp-dir (substitute-in-file-name
-                       "$GOPATH/src/github.com/nsf/gocode/emacs-company")))
-      (when (locate-library "company-go" nil (list go-cp-dir))
-        (add-to-list 'load-path go-cp-dir)
+  ;;
+  ;; Prefer company-mode based auto-completion to auto-complete-mode based one.
+  ;;
+  (cond ((locate-library "company")
+         (add-hook 'go-mode-hook 'cinsk/go-init-cp-autocomplete))
+        ((locate-library "auto-complete")
+         (add-hook 'go-mode-hook 'cinsk/go-init-ac-autocomplete)))
 
-        (cinsk/go-check-gocode)
+  (defun cinsk/go-check-gocode (&optional nowarn)
+    (unless (executable-find "gocode")
+      (add-to-list 'exec-path (substitute-in-file-name "$GOPATH/bin"))
+      (unless (and (executable-find "gocode") nowarn)
+        (warn "gocode not found; try build it using 'go get github.com/nsf/gocode' in $GOPATH directory"))))
 
-        (unless (featurep 'company-go)
-          (require 'company)
-          (require 'company-go)
+  (defun cinsk/go-check-utils (&optional nowarn)
+    (if (executable-find "goimports")
+        (setq gofmt-command "goimports")
+      (add-to-list 'exec-path (substitute-in-file-name "$GOPATH/bin"))
 
-          ))))
-  (set (make-local-variable 'company-backends) '(company-go))
-  (company-mode))
+      (if (executable-find "goimports")
+          (setq gofmt-command "goimports")
+        (unless nowarn
+          (warn "goimports not found; try build it using 'go get golang.org/x/tools/cmd/goimports' in $GOPATH directory")))))
 
-(defun cinsk/go-init-ac-autocomplete ()
-  "Set up auto completion of Go sources.
+  (defun cinsk/go-init-cp-autocomplete-deprecated ()
+    "Set up auto completion of Go sources using `company-mode'"
+    (unless (locate-library "company-go")
+      (let ((go-cp-dir (substitute-in-file-name
+                        "$GOPATH/src/github.com/nsf/gocode/emacs-company")))
+        (when (locate-library "company-go" nil (list go-cp-dir))
+          (add-to-list 'load-path go-cp-dir)
+
+          (cinsk/go-check-gocode)
+
+          (unless (featurep 'company-go)
+            (require 'company)
+            (require 'company-go)
+
+            ))))
+    (set (make-local-variable 'company-backends) '(company-go))
+    (company-mode))
+
+  (defun cinsk/go-init-ac-autocomplete-deprecated ()
+    "Set up auto completion of Go sources.
 
 See https://github.com/auto-complete/auto-complete for more"
-  ;; go-autocomplete.el is distributed with gocode.
-  ;; It seems that I need to manually add gocode into the workspace.
-  ;; The workspace location is specified in the environment, GOPATH.
-  (unless (locate-library "go-autocomplete")
-    (let ((go-ac-dir (substitute-in-file-name
-                       "$GOPATH/src/github.com/nsf/gocode/emacs")))
-      (when (locate-library "go-autocomplete" nil (list go-ac-dir))
-        (add-to-list 'load-path go-ac-dir)
+    ;; go-autocomplete.el is distributed with gocode.
+    ;; It seems that I need to manually add gocode into the workspace.
+    ;; The workspace location is specified in the environment, GOPATH.
+    (unless (locate-library "go-autocomplete")
+      (let ((go-ac-dir (substitute-in-file-name
+                        "$GOPATH/src/github.com/nsf/gocode/emacs")))
+        (when (locate-library "go-autocomplete" nil (list go-ac-dir))
+          (add-to-list 'load-path go-ac-dir)
 
-        (cinsk/go-check-gocode)
+          (cinsk/go-check-gocode)
 
-        (unless (featurep 'go-autocomplete)
-          (require 'go-autocomplete)
-          (require 'auto-complete-config)
-          (ac-config-default)
-          (when (boundp 'go-mode-map)
-            ;; A key binding to force complete (show completion candidate).
-            ;; I thought `ac-complete' would call `ac-complete-go', but
-            ;; `ac-complete' has no effect.
-            (define-key go-mode-map [(control meta ?I)] 'ac-complete-go)))))))
+          (unless (featurep 'go-autocomplete)
+            (require 'go-autocomplete)
+            (require 'auto-complete-config)
+            (ac-config-default)
+            (when (boundp 'go-mode-map)
+              ;; A key binding to force complete (show completion candidate).
+              ;; I thought `ac-complete' would call `ac-complete-go', but
+              ;; `ac-complete' has no effect.
+              (define-key go-mode-map [(control meta ?I)] 'ac-complete-go)))))))
+)
